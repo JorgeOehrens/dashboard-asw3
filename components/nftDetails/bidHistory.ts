82dcd3b86067cce325e0667d48944c516c26fb01
Jorge Oehrens <jorge.oehrens@gmail.com>
2024-04-05 15:36:00 -0300

    Update

--- components/nftDetails/bidHistory.ts
+++ components/nftDetails/bidHistory.ts
@@ -68,6 +68,37 @@ const MarketData = [
         url: "https://www.assetsweb3.com/assets/doc3-35f79f0c.pdf",
       },]
   },
+  {
+    id: 3,
+    img: img2,
+    icon: icon2,
+    name: "Asset web3",
+    symbol: "AW3",
+    price: 900,
+    price_etc: 0.143462934104,
+
+    maxSupply: 1000,
+    adress_token: "0x568A5f0fe54BDFfa0e39bd3dBcF6e56b31CBFdf7",
+    adress_parity: "0xe670B1255d67BC53e68b78A9dC39811Ae3ee1DE6",
+    adress_sales: "0xa13188DFe152793Cd9FC8add0CC843552442D4fE",
+    description:"Assets web 3 description ",
+    documData:[
+      {
+        id: "doc1",
+        title: "Documento 1",
+        url: "https://www.assetsweb3.com/assets/doc1-a6439719.pdf",
+      },
+      {
+        id: "doc2",
+        title: "Documento 2",
+        url: "https://www.assetsweb3.com/assets/doc2-f9347f43.pdf",
+      },
+      {
+        id: "doc3",
+        title: "Documento test",
+        url: "https://www.assetsweb3.com/assets/doc3-35f79f0c.pdf",
+      },]
+  },
 ];
 
 export default MarketData;
