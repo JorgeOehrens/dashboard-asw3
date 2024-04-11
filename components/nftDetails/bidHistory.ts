7bb8f5d076a8f57e5a396afc8cd358cc2a3b3a42
Jorge Oehrens <jorge.oehrens@gmail.com>
2024-04-10 20:17:46 -0400

    Update

--- components/nftDetails/bidHistory.ts
+++ components/nftDetails/bidHistory.ts
@@ -15,9 +15,9 @@ const MarketData = [
     price_etc: 0.08,
 
     maxSupply: 4900,
-    adress_token: "0x745585ed07cde03f319a4541649012aa8ddfcb37",
-    adress_parity: "0x72fefe92a00cb5ea36a44483acc5f08e740b080e",
-    adress_sales: "0xb12e2b53f9850e98aa20222f9dc67845091dfb31",
+    adress_token: "0x698E339981E5720bA826C2134D9ba706ED96Be33",
+    adress_parity: "0xe670B1255d67BC53e68b78A9dC39811Ae3ee1DE6",
+    adress_sales: "0xef23EfEAEDA5478200e6f7A02f4ef5f895E0E5fB",
 
     description:"This property represents 17.52 AC of vacant land as of the effective date of valuation. It is approved for the development of high-density multi-family uses comprised of 480 units demonstrating an overall density of 27.4 units per acre. Ultimately, this project is proposed for the development of one and two bedroom apartment units, along with 432 storage units. The apartment units range in size from 658 to 1,252 square feet with an average size at 827 square feet. Storage units includes a mix of 106-4’ x 8’ and 326 - 6’ x 8’ units. ",
     documData:[
@@ -81,6 +81,37 @@ const MarketData = [
     adress_token: "0x568A5f0fe54BDFfa0e39bd3dBcF6e56b31CBFdf7",
     adress_parity: "0xe670B1255d67BC53e68b78A9dC39811Ae3ee1DE6",
     adress_sales: "0xa13188DFe152793Cd9FC8add0CC843552442D4fE",
+    description:"Assets web 3 description ",
+    documData:[
+      {
+        id: "doc1",
+        title: "Documento 1",
+        url: "https://www.assetsweb3.com/assets/doc1-a6439719.pdf",
+      },
+      {
+        id: "doc2",
+        title: "Documento 2",
+        url: "https://www.assetsweb3.com/assets/doc2-f9347f43.pdf",
+      },
+      {
+        id: "doc3",
+        title: "Documento test",
+        url: "https://www.assetsweb3.com/assets/doc3-35f79f0c.pdf",
+      },]
+  },
+  {
+    id: 4,
+    img: img2,
+    icon: icon2,
+    name: "TokenTest",
+    symbol: "Test",
+    price: 900,
+    price_etc: 0.143462934104,
+
+    maxSupply: 2,
+    adress_token: "0x698E339981E5720bA826C2134D9ba706ED96Be33",
+    adress_parity: "0xe670B1255d67BC53e68b78A9dC39811Ae3ee1DE6",
+    adress_sales: "0xef23EfEAEDA5478200e6f7A02f4ef5f895E0E5fB",
     description:"Assets web 3 description ",
     documData:[
       {
